4a2ad6f1032ac2b3ea0881aadb7670f98e943efa
D4ROVAN1E <garomiroved@mail.ru>
2025-08-26 16:07:22 +0700

    PR2 deleted PR2.cpp

---
+++ PR2/PR2/old ver/PR2.cpp
@@ -0,0 +1,24 @@
+﻿/*#include "ExtendedEuclidean.h"
+#include "ModuloComparison.h"
+#include "Shamir.h"
+#include "Fraction.h"
+#include "Attack.h"
+
+// Перегрузка оператора для красивого вывода вектора uint8_t (как строку)
+ostream& operator<<(ostream& os, const vector<uint8_t>& vec) {
+	for (uint8_t element : vec) {
+		os << static_cast<char>(element);
+	}
+	return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, vector<T> vec) {
+	for (T element : vec) {
+		os << element << " ";
+	}
+	os << "\n";
+	return os;
+}
+
+*/
