278b98bc63891374f2796001c8ccf8d55ab10565
D4ROVAN1E <garomiroved@mail.ru>
2025-08-28 14:45:55 +0700

    Bifid ver 1

---
+++ RGR2/RGR2/Bifid.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+using namespace std;
+
+uint8_t get_row_from_char(uint8_t char_code);
+
+uint8_t get_col_from_char(uint8_t char_code);
+
+char get_char_from_coords(uint8_t row, uint8_t col);
+
+string bifid_encrypt(const string& plaintext);
+
+string bifid_decrypt(const string& ciphertext);
---
+++ RGR2/RGR2/RGR2.cpp
@@ -0,0 +1,31 @@
+﻿/*Шифр Бифида, Шифр «железнодорожной изгороди», Аффинный шифр
+70 % 16 + 1 - номер столбца от кода буквы
+70 / 16 + 1 - номер строки от кода буквы
+CP866*/
+#include "Bifid.h"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    cout << "Введите текст для шифрования: ";
+    string message;
+    getline(cin, message);
+
+    if (message.empty()) {
+        cout << "Введена пустая строка. Завершение программы." << endl;
+        return 0;
+    }
+
+    cout << "\nОригинальный текст: " << message << endl;
+
+    string encrypted_message = bifid_encrypt(message);
+    cout << "Зашифрованный текст: " << encrypted_message << endl;
+
+    string decrypted_message = bifid_decrypt(encrypted_message);
+    cout << "Расшифрованный текст: " << decrypted_message << endl;
+
+    cout << "\nПроверка: " << (message == decrypted_message ? "Успешно!" : "Ошибка!") << endl;
+}
